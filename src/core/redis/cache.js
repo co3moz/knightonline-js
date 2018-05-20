a1d7acc2f3787405861f0d8e9759be5f36c7d6f0
Doğan Derya <doganderya59@gmail.com>
2018-05-20 19:42:12 +0300

    locking for cache

--- src/core/redis/cache.js
+++ src/core/redis/cache.js
@@ -7,7 +7,45 @@ module.exports = async function (name, fn, timeout) {
     return JSON.parse(data);
   }
 
-  data = await fn();
-  await redis.setAsync('cache-' + name, JSON.stringify(data), 'EX', timeout || 60);
+  if (await waitLock(name)) {
+    let data = await redis.getAsync('cache-' + name);
+
+    if (data) {
+      return JSON.parse(data);
+    }
+  }
+
+  lock(name);
+
+  try {
+    data = await fn();
+    await redis.setAsync('cache-' + name, JSON.stringify(data), 'EX', timeout || 60);
+  } finally {
+    unlock(name);
+  }
+
   return data;
+}
+
+const _locks = {};
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+async function waitLock(name) {
+  if (!_locks[name]) {
+    return false;
+  }
+
+  for (; ;) {
+    await delay(250);
+    if (!_locks[name]) {
+      return true;
+    }
+  }
+}
+
+function lock(name) {
+  _locks[name] = true;
+}
+
+function unlock(name) {
+  _locks[name] = false;
 }
