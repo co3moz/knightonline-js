051d5d19ef4f4cc96087eb198eb48d05fbdd6de9
Furkan Başaran <frknbasaran@gmail.com>
2018-09-28 03:36:55 +0300

    [game_server] GM chat commands support.
    - GM can run commands from chat input now.
    - Try type "+WAR Burası adapazarı ulan!" into chat input with character who has GM rights.

--- src/game_server/ops/CHAT.js
+++ src/game_server/ops/CHAT.js
@@ -1,12 +1,34 @@
 const unit = require('../../core/utils/unit');
 const crypt = require('../../core/utils/crypt');
 const config = require('config');
+const gmCommands = require('../../game_server/var/gm_commands');
 
 module.exports = async function ({ body, socket, opcode }) {
   let type = body.byte();
   let message = body.string();
+  // is message contain '+' sign and character has gm rights?
+  let IS_GM_COMMAND = message.substr(0, 1) === '+' && !socket.character.gm;
 
-  if (type == MESSAGE_TYPES.GENERAL) {
+  if (IS_GM_COMMAND) {
+    // get first 3 characters for gm chat command
+    let command = message.substr(1,3);
+
+    if (message.length > 128) {
+      message = message.substring(0, 128)
+    }
+
+    socket.shared.region.regionSend(socket, [
+      opcode,
+      gmCommands[command],
+      socket.user.nation,
+      ...unit.short(socket.session & 0xFFFF),
+      ...unit.byte_string(socket.character.name),
+      ...unit.string(message.substr(4, message.length), 'ascii')
+    ]);
+  }
+
+  // don't send message as public if it is valid gm command
+  if (!IS_GM_COMMAND && type == MESSAGE_TYPES.GENERAL) {
     if (message.length > 128) {
       message = message.substring(0, 128)
     }
@@ -20,6 +42,7 @@ module.exports = async function ({ body, socket, opcode }) {
       ...unit.string(message, 'ascii')
     ]);
   }
+
 }
 
 const MESSAGE_TYPES = {
---
+++ src/game_server/var/gm_commands.js
@@ -0,0 +1,29 @@
+module.exports = {
+    GEN:1,	// General
+    PRV:2,	// Private
+    PRT:3,	// Party
+    FRC:4,	// Force
+    SHT:5,	// SHOUT
+    KNG:6,	// KNIGHTS
+    PUB:7,	// PUBLIC
+    WAR:8,	// WAR
+    PRM:9,	// PERMAMENT
+    EPR:10,	// END PERMAMENT
+    MON:11,	// MONUMENT_NOTICE
+    GMC:12,	// GM CHAT
+    CMD:13,	// COMMANDER
+    MRC:14,	// MERCHANT
+    ALY:15,	// ALLIANCE	
+    ANN:17,	// ANNOUNCEMENT
+    SKP:19,	// SEEKING PARTY CHAT
+    GMI:21,    // info window : "Level: 0, UserCount:16649" (NOTE: Think this is the missing overhead info (probably in form of a command (with args))
+    CMP:22,    // Commander Chat PM??
+    CLN:24,	// CLAN NOTICE
+    KRW:25,	// KROWAZ NOTICE
+    DTN:26,	// DEATH NOTICE
+    CSE:27,    // The enemy has destroyed the Chaos stone something (Red text, middle of screen)
+    CSN:28,	// CHAOS STONE NOTICE
+    ANE:29,    // what's it used for?
+    CHR:33,	// CHATROOM NOTICE
+    NKN:34	// NOAH KNIGHTS NOTICE
+};
